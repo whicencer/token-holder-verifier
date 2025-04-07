a8de80b577a71fb5c1a7392514c2a2f7476a3da0
Deniel <denielaz2020@gmail.com>
2025-04-07 02:12:01 +0200

    refactor: update scheduled verification job to use Bottleneck for rate limiting and enhance user processing

--- server/src/cron/scheduledVerification.ts
+++ server/src/cron/scheduledVerification.ts
@@ -2,27 +2,38 @@ import { CronJob } from 'cron';
 import { UserRepository } from '../../database/User';
 import { verifier } from '../services/verifier';
 import BotInstance from '../bot';
+import Bottleneck from 'bottleneck';
+
+const limiter = new Bottleneck({
+	maxConcurrent: 1,
+	minTime: 200
+});
 
 export const scheduledVerificationJob = new CronJob(
 	'*/30 * * * * *',
 	async () => {
-		console.log("Running scheduled verification job...");
-		const bot = new BotInstance();
 		try {
+			const bot = new BotInstance();
 			const userRepository = new UserRepository();
-			const users = await userRepository.getRecentCheckedUsersBatch(50);
+			const users = await userRepository.getRecentCheckedUsersBatch(100); // Will handle 100 users at ~`80 seconds
 			
 			if (users.length > 0) {
-				users.forEach(async (user) => {
-					if (user.tonAddress) {
-						const { message, verified } = await verifier.verifyWallet(user.userId, user.tonAddress);
-						if (!verified && user.joinedChannelId) {
-							await bot.kickChatMember(user.joinedChannelId, user.userId);
-							await bot.sendMessage(user.userId, `${message} \n\n You were kicked from the group.`, { parse_mode: "Markdown" });
-							await userRepository.setAttribute(user.userId, "verified", false);
+				const start = Date.now();
+				console.log(`Starting test batch of ${users.length} users...`);
+				for (const user of users) {
+					await limiter.schedule(async () => {
+						if (user.tonAddress) {
+							const { message, verified } = await verifier.verifyWallet(user.userId, user.tonAddress);
+							if (!verified && user.joinedChannelId) {
+								await bot.kickChatMember(user.joinedChannelId, user.userId);
+								await bot.sendMessage(user.userId, `${message} \n\n You were kicked from the group.`, { parse_mode: "Markdown" });
+								await userRepository.setAttribute(user.userId, "verified", false);
+							}
 						}
-					}
-				});
+					});
+				}
+				const end = Date.now();
+				console.log(`✅ Finished in ${(end - start) / 1000}s`);
 			}
 		} catch (error) {
 			console.error("Error in scheduled verification job:", error);
